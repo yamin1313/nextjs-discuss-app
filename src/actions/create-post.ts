7bdf8eb460e369b39c402ebbdbf4f8142678da64
Azizi <Azizi.SeyedYamin@tapsi.food>
2025-04-15 12:28:20 +0330

    feat(create-post): implement create post functionality

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,5 +1,91 @@
 "use server";
 
-export async function createPost() {
-  //TODO: revalidate the topic showPage
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/path";
+import { Post } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { title } from "process";
+import { z } from "zod";
+
+const CreatePostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  slug: string,
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = CreatePostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed into do this"],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ["Can not find topic."],
+      },
+    };
+  }
+
+  let post: Post;
+  try {
+    post = await db.post.create({
+      // @ts-ignore
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        userId: session.user.id,
+        topicId: topic.id,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Failed to create post"],
+        },
+      };
+    }
+  }
+
+  revalidatePath(paths.topicShowPath(slug));
+  redirect(paths.postShowPath(slug, post.id));
 }
--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import PostCreateForm from "@/components/posts/post-create-form";
+
 interface TopicShowPageProps {
   params: {
     slug: string;
@@ -6,12 +8,15 @@ interface TopicShowPageProps {
 
 export default function TopicShowPage({ params }: TopicShowPageProps) {
   const { slug } = params;
+
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
       </div>
-      <div></div>
+      <div>
+        <PostCreateForm slug={slug} />
+      </div>
     </div>
   );
 }
